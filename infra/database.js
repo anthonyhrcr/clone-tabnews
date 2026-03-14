3b698bf8e962f2ba6d3f81ffcd307c2eac7d8be7
anthonyhrcr <anthonyhenrique408@gmail.com>
2026-03-14 02:28:19 +0000

    add `database.js` and `env` files

---
+++ infra/database.js
@@ -0,0 +1,22 @@
+import { handleSmoothScroll } from "next/dist/shared/lib/router/router";
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
+
+// digitar o comando com o espaço na frente do terminal faz com que ele não fique no historico
--- pages/api/v1/status/status.js
+++
@@ -1,5 +0,0 @@
-function status(request, response) {
-  response.status(200).json({ são: "quase 5:00 da manhã" });
-}
-
-export default status;
