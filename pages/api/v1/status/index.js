79de10c8fd6cf58bef1ed416b39361de8a4c1746
anthonyhrcr <anthonyhenrique408@gmail.com>
2026-03-24 11:58:44 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ são: "quase 5:00 da manhã" });
+  try {
+    const updatedAt = new Date().toISOString();
+
+    const versionResult = await database.query("SHOW server_version;");
+    const maxConnectionsResult = await database.query("SHOW max_connections;");
+    const currentConnectionsResult = await database.query(
+      "SELECT count(*) FROM pg_stat_activity;",
+    );
+    const databaseName = process.env.POSTGRES_DB;
+    const openedConnectionsValue = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+      values: [databaseName],
+    });
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      postgres: {
+        version: versionResult.rows[0].server_version,
+        max_connections: Number(maxConnectionsResult.rows[0].max_connections),
+        current_connections: Number(currentConnectionsResult.rows[0].count),
+        opened_connections: Number(openedConnectionsValue.rows[0].count),
+      },
+    });
+  } catch (err) {
+    console.error("Erro no /status:", err);
+    response.status(500).json({ error: err.message });
+  }
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,4 +3,23 @@ const { RESPONSE_LIMIT_DEFAULT } = require("next/dist/server/api-utils");
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.postgres.version).toEqual("16.0");
+  expect(responseBody.postgres.max_connections).toEqual(100);
+  expect(responseBody.postgres.current_connections).toBeGreaterThanOrEqual(1);
+  expect(responseBody.postgres.opened_connections).toEqual(1);
+
+  console.log(parsedUpdatedAt);
+  console.log("Postgres version:", responseBody.postgres.version);
+  console.log("Max connections:", responseBody.postgres.max_connections);
+  console.log(
+    "Current connections:",
+    responseBody.postgres.current_connections,
+  );
+  console.log("Opened connections:", responseBody.postgres.opened_connections);
 });
