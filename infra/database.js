c90f05b400a4a7d453c0ae3076b1bf85cbfb5599
anthonyhrcr <anthonyhenrique408@gmail.com>
2026-04-15 00:29:59 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,3 @@
-import { handleSmoothScroll } from "next/dist/shared/lib/router/router";
 import { Client } from "pg";
 
 async function query(queryObject) {
@@ -8,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   await client.connect();
 
